7a157d001ec89a1ce0ee55c6ce7c38adc18396bc
Jesse Lind <Jesseclind@gmail.com>
2023-01-23 14:42:15 -0500

    build: add units list

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { units } from './units.js'
+
+const converter = (quantity: number, from: string, to: string = 'grams') => {
+  console.log(quantity, from, to)
+
+  // units.map
+  return 'test'
+}
+
+export default converter
